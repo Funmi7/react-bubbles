a6b60c04de9e63551572aff5fe6e787dec3c500c
Funmi7 <talabifunmilayo@gmail.com>
2019-10-18 10:04:10 +0100

    Built login component with authentication and rendered the bubblepages with the fetched data

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, NavLink, withRouter, Redirect } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage';
 
-function App() {
+function App(props) {
+  const onLogout = () => {
+    localStorage.clear();
+    props.history.replace('/')
+  }
   return (
-    <Router>
+    <div>
+      {/* <nav>
+        <NavLink exact to ='/'>Login</NavLink>
+        <NavLink to ='/bubblesPage'>Bubbles</NavLink>
+      </nav> */}
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Route path='/bubblePage' 
+        render={props => withAthCheck(BubblePage, props)}/>
       </div>
-    </Router>
+    </div>
   );
 }
 
-export default App;
+function withAthCheck(Component, props) {
+  if (localStorage.getItem('token')) {
+    return <Component {...props} />
+  }
+  return <Redirect to='/' />;
+}
+
+export default withRouter(App);
+
+
